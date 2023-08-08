a38bdfdf5db9fd22f78c37b7ea65011a32ffc90f
Cwait00 <cwaitanobongoza@gmail.com>
2023-08-08 14:50:25 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-main.c
+++ 0x0B-malloc_free/101-main.c
@@ -18,7 +18,7 @@ void print_tab(char **tab)
 }
 
 /**
- * main - strtow check the code for ALX School students.
+ * main - check the code for ALX School students.
  * Return: 1 if an error occurred, 0 otherwise
  */
 int main(void)
