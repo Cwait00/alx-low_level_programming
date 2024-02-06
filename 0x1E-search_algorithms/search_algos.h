a23f6e95f468434308a1a79ff7b7e53603174898
Cwait00 <cwaitanobongoza@gmail.com>
2024-02-06 02:48:53 -0800

    updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,6 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int **allocate_map(int n, int m);
 
 #endif /* SEARCH_ALGOS_H */
