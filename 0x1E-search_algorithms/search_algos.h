42748ccf3cb0da7251f1068f87915d07dcf84a3d
Cwait00 <cwaitanobongoza@gmail.com>
2024-02-07 00:25:07 -0800

    104-advanced_binary.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,7 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 void print_subarray(int *array, size_t low, size_t high);
+int advanced_binary(int *array, size_t size, int value);
+int binary_search_recursive(int *array, size_t start, size_t end, int value);
 
 #endif /* SEARCH_ALGOS_H */
