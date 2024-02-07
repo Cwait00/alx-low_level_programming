16ef7fb3ac356dc7f1778b7b1d22794ab79580f6
Cwait00 <cwaitanobongoza@gmail.com>
2024-02-07 02:31:33 -0800

    updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,12 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-#include <stddef.h>
+#include <stddef.h> /* For size_t */
+#include "listint.h"
 
+
+/* Function prototypes */
+listint_t *jump_list(listint_t *list, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t low, size_t high, int value);
 int **allocate_map(int n, int m);
